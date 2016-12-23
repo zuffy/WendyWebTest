bbae92edafd58a698e83733616ffd011618d8676
zuffy <zuffy.ma@SuperD3D.com>
2016-12-23 12:37:14 +0800

    补充内容
    Signed-off-by: zuffy <zuffy.ma@SuperD3D.com>

--- wendy_new/js/constant.js
+++ wendy_new/js/constant.js
@@ -12,17 +12,17 @@
 				{
 					"type": TypePicCollection,
 					"title": "flag.jpg",
-					"url":"images/bigImg/flag.jpg",
+					"url":"images/big/flag.jpg",
 					"snapsrc": "images/collection/printright1.jpg"
 				},{
 					"type": TypePicCollection,
 					"title": "cross.jpg",
-					"url": "images/bigImg/cross.jpg",
+					"url": "images/big/cross.jpg",
 					"snapsrc": "images/collection/printright2.jpg"
 				},{
 					"type": TypePicCollection,
 					"title": "fullstop.jpg",
-					"url":"images/bigImg/fullstop.jpg",
+					"url":"images/big/fullstop.jpg",
 					"snapsrc": "images/collection/printright3.gif"
 				}
 
@@ -48,22 +48,22 @@
 				{
 					"type": TypePicCollection,
 					"title": "ArielDefence Crab.jpg",
-					"url":"images/bigImg/ArielDefence Crab.jpg",
+					"url":"images/big/ArielDefence Crab.jpg",
 					"snapsrc": "images/collection/arielcrab1.gif"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Spaghetti.jpg",
-					"url": "images/bigImg/ArielDefence Spaghetti.jpg",
+					"url": "images/big/ArielDefence Spaghetti.jpg",
 					"snapsrc": "images/collection/arielcrab2.gif"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Hotdog.jpg",
-					"url":"images/bigImg/ArielDefence Hotdog.jpg",
+					"url":"images/big/ArielDefence Hotdog.jpg",
 					"snapsrc": "images/collection/arielcrab3.jpg"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Burger.jpg",
-					"url":"images/bigImg/ArielDefence Burger.jpg",
+					"url":"images/big/ArielDefence Burger.jpg",
 					"snapsrc": "images/collection/arielcrab4.jpg"
 				}
 
@@ -89,27 +89,27 @@
 				{
 					"type": TypePicCollection,
 					"title": "Window",
-					"url":"images/bigImg/Soufeel window.jpg",
+					"url":"images/big/Soufeel window.jpg",
 					"snapsrc": "images/collection/soufeel_windown.png"
 				},{
 					"type": TypePicCollection,
 					"title": "DIY area",
-					"url": "images/bigImg/Soufeel diywall.jpg",
+					"url": "images/big/Soufeel diywall.jpg",
 					"snapsrc": "images/collection/soufeel2.png"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Hotdog.jpg",
-					"url":"images/bigImg/Soufeel crystal.jpg",
+					"url":"images/big/Soufeel crystal.jpg",
 					"snapsrc": "images/collection/soufeel3.png"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Burger.jpg",
-					"url":"images/bigImg/Soufeel KV.jpg",
+					"url":"images/big/Soufeel KV.jpg",
 					"snapsrc": "images/collection/soufeel4.png"
 				},{
 					"type": TypePicCollection,
 					"title": "ArielDefence Burger.jpg",
-					"url":"images/bigImg/Soufeel logo.jpg",
+					"url":"images/big/Soufeel logo.jpg",
 					"snapsrc": "images/collection/soufeel5.png"
 				}
 
@@ -136,18 +136,18 @@
 				{
 					"type": TypePicCollection,
 					"title": "flag.jpg",
-					"url":"images/bigImg/flag.jpg",
-					"snapsrc": "images/collection/printright1.jpg"
+					"url":"images/big/Tide light1.jpg",
+					"snapsrc": "images/collection/tideright_1.gif"
 				},{
 					"type": TypePicCollection,
 					"title": "cross.jpg",
-					"url": "images/bigImg/cross.jpg",
-					"snapsrc": "images/collection/printright2.jpg"
+					"url": "images/big/Tide light2.jpg",
+					"snapsrc": "images/collection/tideright_2.gif"
 				},{
 					"type": TypePicCollection,
 					"title": "fullstop.jpg",
-					"url":"images/bigImg/fullstop.jpg",
-					"snapsrc": "images/collection/printright3.gif"
+					"url":"images/big/Tide light3.jpg",
+					"snapsrc": "images/collection/tideright_3.gif"
 				}
 
 			],
@@ -163,7 +163,85 @@
 			},
 
 			"descTitle": "Tide",
-			"desc": "<p>Idea:Dandruff surrender to Head & Shoulders.</p> <p>This campaign win the first cannes in China.</p>"
+			"desc": "<p>Idea:Tide gives you the brighter wash.</p>\
+			<p>Visual is made by real light tube. light bulb and fluorescent lamp.</p>"
+		}
+
+		,"reborn": {
+
+			"urls": [
+				{
+					"type": TypePicCollection,
+					"title": "flag.jpg",
+					"url":"images/big/Tide light1.jpg",
+					"snapsrc": "images/collection/reborn1.png"
+				},{
+					"type": TypePicCollection,
+					"title": "cross.jpg",
+					"url": "images/big/Tide light2.jpg",
+					"snapsrc": "images/collection/reborn2.png"
+				},{
+					"type": TypePicCollection,
+					"title": "fullstop.jpg",
+					"url":"images/big/H&S Reborn billboard.jpg",
+					"snapsrc": "images/collection/reborn3.png"
+				},{
+					"type": TypePicCollection,
+					"title": "fullstop.jpg",
+					"url":"images/big/H&S Reborn KV1.jpg",
+					"snapsrc": "images/collection/reborn4.png"
+				}
+
+			],
+
+			"awards": {
+				"desc": "",
+				"urls":[]
+			},
+
+			"descTitle": "Head & Shouders",
+			"desc": "<p>Brief:Promote H&S new bottle</p>\
+			<p>We think this is a chance for H&S to rejuvenate its image.</p>\
+			<p>Idea:Metermorphosis</p>"
+		}
+
+		,"kfc": {
+
+			"urls": [
+				{
+					"type": TypePicCollection,
+					"title": "Case Study",
+					"url":"images/big/Tide light1.jpg",
+					"snapsrc": "images/collection/vote1.png"
+				},{
+					"type": TypePicCollection,
+					"title": "The Fight",
+					"url": "images/big/Tide light2.jpg",
+					"snapsrc": "images/collection/vote2.png"
+				},{
+					"type": TypePicCollection,
+					"title": "Chen Kun & original",
+					"url":"images/big/H&S Reborn billboard.jpg",
+					"snapsrc": "images/collection/vote3.png"
+				},{
+					"type": TypePicCollection,
+					"title": "Ke Zhen Dong & crispy",
+					"url":"images/big/H&S Reborn KV1.jpg",
+					"snapsrc": "images/collection/vote4.png"
+				}
+
+			],
+
+			"awards": {
+				"desc": "",
+				"urls":[]
+			},
+
+			"descTitle": "KFC - Vote",
+			"desc": "<p>2012 KFC was flooded with negative news.How can we bring consumer attention back to their product?</p>\
+			<p>The last launch of crispy chicken replacing the original in test market has brought complain.</p>\
+			<p>This time asked consumer to vote which one they want, crispy or original?</p>\
+			<p>This was the first time people in China could vote!It successfully create lots of noise and once again bring focus back to the product.</p>"
 		}
 
 	}
