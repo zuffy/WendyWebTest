29430cd401afbc5f2475c1dbc2e17dcf7658707d
zuffy <zuffy.ma@SuperD3D.com>
2016-12-24 14:58:17 +0800

    内容调整;
    Signed-off-by: zuffy <zuffy.ma@SuperD3D.com>

--- wendy_new/js/constant.js
+++ wendy_new/js/constant.js
@@ -11,17 +11,17 @@
 			"urls": [
 				{
 					"type": TypePicCollection,
-					"title": "flag.jpg",
+					"title": "",
 					"url":"images/big/flag.jpg",
 					"snapsrc": "images/collection/printright1.jpg"
 				},{
 					"type": TypePicCollection,
-					"title": "cross.jpg",
+					"title": "",
 					"url": "images/big/cross.jpg",
 					"snapsrc": "images/collection/printright2.jpg"
 				},{
 					"type": TypePicCollection,
-					"title": "fullstop.jpg",
+					"title": "",
 					"url":"images/big/fullstop.jpg",
 					"snapsrc": "images/collection/printright3.gif"
 				}
@@ -47,22 +47,22 @@
 			"urls": [
 				{
 					"type": TypePicCollection,
-					"title": "ArielDefence Crab.jpg",
+					"title": "",
 					"url":"images/big/ArielDefence Crab.jpg",
 					"snapsrc": "images/collection/arielcrab1.gif"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Spaghetti.jpg",
+					"title": "",
 					"url": "images/big/ArielDefence Spaghetti.jpg",
 					"snapsrc": "images/collection/arielcrab2.gif"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Hotdog.jpg",
+					"title": "",
 					"url":"images/big/ArielDefence Hotdog.jpg",
 					"snapsrc": "images/collection/arielcrab3.jpg"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Burger.jpg",
+					"title": "",
 					"url":"images/big/ArielDefence Burger.jpg",
 					"snapsrc": "images/collection/arielcrab4.jpg"
 				}
@@ -98,17 +98,17 @@
 					"snapsrc": "images/collection/soufeel2.png"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Hotdog.jpg",
+					"title": "Crystal pole",
 					"url":"images/big/Soufeel crystal.jpg",
 					"snapsrc": "images/collection/soufeel3.png"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Burger.jpg",
+					"title": "KV",
 					"url":"images/big/Soufeel KV.jpg",
 					"snapsrc": "images/collection/soufeel4.png"
 				},{
 					"type": TypePicCollection,
-					"title": "ArielDefence Burger.jpg",
+					"title": "Logo",
 					"url":"images/big/Soufeel logo.jpg",
 					"snapsrc": "images/collection/soufeel5.png"
 				}
@@ -135,17 +135,17 @@
 			"urls": [
 				{
 					"type": TypePicCollection,
-					"title": "flag.jpg",
+					"title": "",
 					"url":"images/big/Tide light1.jpg",
 					"snapsrc": "images/collection/tideright_1.gif"
 				},{
 					"type": TypePicCollection,
-					"title": "cross.jpg",
+					"title": "",
 					"url": "images/big/Tide light2.jpg",
 					"snapsrc": "images/collection/tideright_2.gif"
 				},{
 					"type": TypePicCollection,
-					"title": "fullstop.jpg",
+					"title": "",
 					"url":"images/big/Tide light3.jpg",
 					"snapsrc": "images/collection/tideright_3.gif"
 				}
@@ -171,23 +171,23 @@
 
 			"urls": [
 				{
-					"type": TypePicCollection,
-					"title": "flag.jpg",
-					"url":"images/big/Tide light1.jpg",
+					"type": TypeVideo,
+					"title": "TVC teaser",
+					"url":"mp4/H&Sreborn teaser.mp4",
 					"snapsrc": "images/collection/reborn1.png"
 				},{
-					"type": TypePicCollection,
-					"title": "cross.jpg",
-					"url": "images/big/Tide light2.jpg",
+					"type": TypeVideo,
+					"title": "TVC launch",
+					"url": "mp4/H&Sreborn.mp4",
 					"snapsrc": "images/collection/reborn2.png"
 				},{
 					"type": TypePicCollection,
-					"title": "fullstop.jpg",
+					"title": "Outdoor poster",
 					"url":"images/big/H&S Reborn billboard.jpg",
 					"snapsrc": "images/collection/reborn3.png"
 				},{
 					"type": TypePicCollection,
-					"title": "fullstop.jpg",
+					"title": "KV",
 					"url":"images/big/H&S Reborn KV1.jpg",
 					"snapsrc": "images/collection/reborn4.png"
 				}
@@ -209,24 +209,24 @@
 
 			"urls": [
 				{
-					"type": TypePicCollection,
+					"type": TypeVideo,
 					"title": "Case Study",
-					"url":"images/big/Tide light1.jpg",
+					"url":"mp4/KFCVSCasestudy.mp4",
 					"snapsrc": "images/collection/vote1.png"
 				},{
-					"type": TypePicCollection,
+					"type": TypeVideo,
 					"title": "The Fight",
-					"url": "images/big/Tide light2.jpg",
+					"url": "mp4/KFCVSinternetFight.mp4",
 					"snapsrc": "images/collection/vote2.png"
 				},{
-					"type": TypePicCollection,
+					"type": TypeVideo,
 					"title": "Chen Kun & original",
-					"url":"images/big/H&S Reborn billboard.jpg",
+					"url":"mp4/KFCVSTvcOriginal.mp4",
 					"snapsrc": "images/collection/vote3.png"
 				},{
-					"type": TypePicCollection,
+					"type": TypeVideo,
 					"title": "Ke Zhen Dong & crispy",
-					"url":"images/big/H&S Reborn KV1.jpg",
+					"url":"mp4/KFCVSTvcCrispy.mp4",
 					"snapsrc": "images/collection/vote4.png"
 				}
 
@@ -242,6 +242,169 @@
 			<p>The last launch of crispy chicken replacing the original in test market has brought complain.</p>\
 			<p>This time asked consumer to vote which one they want, crispy or original?</p>\
 			<p>This was the first time people in China could vote!It successfully create lots of noise and once again bring focus back to the product.</p>"
+		}
+
+		,"revamp": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "Case Study",
+					"url":"mp4/KFCMenuRevampCasestudy.mp4",
+					"snapsrc": "images/collection/revamp1.jpg"
+				},{
+					"type": TypeVideo,
+					"title": "Product TVC",
+					"url": "mp4/KFCMenuRevampProduct.mp4",
+					"snapsrc": "images/collection/revamp2.jpg"
+				},{
+					"type": TypeVideo,
+					"title": "WuMoChou & snack",
+					"url":"mp4/KFCMenuRevampWuMoChou.mp4",
+					"snapsrc": "images/collection/revamp3.jpg"
+				},{
+					"type": TypeVideo,
+					"title": "WuMoChou, city invitation",
+					"url":"mp4/KFCMenuRevampWuMoChouInvitation.mp4",
+					"snapsrc": "images/collection/revamp4.jpg"
+				}
+
+			],
+
+			"awards": {
+				"desc": "",
+				"urls":[]
+			},
+
+			"descTitle": "KFC - Menu Revamp",
+			"desc": "<p>Brief:Launch KFC New Menu.</p>\
+			<p>We think this is a good chance to make a bold statement that KFC have changed.</p>\
+			<p>The first time in history of KFC having 4 celebrities, each representing one category of food.</p>\
+			<p>They competed who got more LIKE from fans.</p>"
+		}
+
+		,"trediano": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "Online Video",
+					"url":"mp4/Trendiano.mp4",
+					"snapsrc": "images/collection/onlineVideo.jpg"
+				},{
+					"type": TypePicCollection,
+					"title": "H5",
+					"url":"images/big/tredianoh5.jpg",
+					"snapsrc": "images/collection/h5.jpg"
+				}
+
+			],
+
+			"awards": {
+				"desc": "",
+				"urls":[]
+			},
+
+			"descTitle": "TRENDIANO",
+			"desc": "<p>Brief:Promote Trendiano, a new fashion brand</p>\
+			<p>The real challenge is no one knows how to pronounce Trendiano</p>\
+			<p>Idea:Only cool people can pronounce Trendiano</p>\
+			<p>The music video goes with H5, people are asked to pronounce Trendiano and win prize.</p>"
+		}
+
+		,"volleyball": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "",
+					"url": "mp4/Oil Olympic Volleyball.mp4", // 'tt.mp4', //
+					"snapsrc": ""
+				}
+			],
+
+			"awards": {
+				"desc": "TVC",
+				"urls":[]
+			},
+
+			"descTitle": "Arawana Oil",
+			"desc": "<p>Brief:Arawana Oil sponsoring China volleyball in 2008.</p>\
+			<p>There are two challenge here, 1. how cooking related to sport 2. what if they loose</p>\
+			<p>Idea:Real support is no matter when you win or when you fall. Add oil!(in Chinese it meens support)</p>"
+		}
+
+		,"kotex": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "",
+					"url": "mp4/Kotex.mp4", // 'tt.mp4', //
+					"snapsrc": ""
+				}
+			],
+
+			"awards": {
+				"desc": "TVC",
+				"urls":[]
+			},
+
+			"descTitle": "KOTEX",
+			"desc": "<p>TVC in sanitary category are always blue & white heerful girls jumping, its almost embarrass to watch.</p>\
+			<p>Kotex is about woman that have attitude and style. They don't compromise Their packaging is black.</p>\
+			<p>This is the 1st B/W TVC in this category.</p>"
+		}
+
+		,"hsmethol": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "",
+					"url": "mp4/HSCoolWay.mp4", // 'tt.mp4', //
+					"snapsrc": ""
+				}
+			],
+
+			"awards": {
+				"desc": "TVC",
+				"urls":[]
+			},
+
+			"descTitle": "HEAD & SHOULDERS",
+			"desc": "<p>Brief:Promote menthol shampoo to young peopel.</p>\
+			<p>Challenge:How to make H&S appeal to young people?</p>\
+			<p>Idea:Remove dandruff in a cool way. H&S or no other way.</p>\
+			<p>This campaign started in China and later also ran in Europe, Asia Pacific and USA.</p>"
+		}
+
+		,"bigstain": {
+
+			"urls": [
+				{
+					"type": TypeVideo,
+					"title": "",
+					"url": "mp4/ArielBigStainEng.mp4", // 'tt.mp4', //
+					"snapsrc": ""
+				}
+			],
+
+			"awards": {
+				"desc": "",
+				"urls":[
+					{"url": "images/award/cannes.png"},
+					{"url": "images/award/Spikes_Asia.gif"},
+					{"url": "images/award/onshow.png"},
+					{"url": "images/award/Longxi.png"},
+					{"url": "images/award/greatwall.png"}
+				]
+			},
+
+			"descTitle": "Ariel - Big Stain",
+			"desc": "<p>Brief:Create a poster to promote a new product.</p>\
+			<p>This new product could remove stain so easy that it made cleaning fun.</p>\
+			<p>We created an installation of staining & removing stain.for consumer to experience.</p>"
 		}
 
 	}
